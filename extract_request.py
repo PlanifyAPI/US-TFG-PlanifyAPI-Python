3ff9a1eb85b55a083c9c3a9b4d33aa05736f84f6
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2024-04-03 16:31:41 +0200

    feat: Extract requests from logs

--- extract_request.py
+++ extract_request.py
@@ -1,20 +1,41 @@
-import re
+"""
+This module provides functions for filtering logs and extracting requests.
+"""
+
 import os
 
-# Directorio que contiene los archivos de logs
-directorio_logs = '../tfg/log/amadeus__r_ft_/log.json'
 
-# Función para leer y filtrar los archivos de logs
-def filtrar_logs(archivo):
-    with open(archivo, 'r') as f:
-        with open('requests.log', 'w') as f_out:
-            for linea in f:
-                print(linea)
-                if "GET" in linea or "HTTP/1.1" in linea:
-                    f_out.write(linea)
+def filter_logs(file: str) -> None:
+    """
+    Filters the logs in the given file and writes the filtered logs to a new file.
 
-# Recorrer todos los archivos en el directorio de logs
-for archivo in os.listdir(directorio_logs):
-    ruta_archivo = os.path.join(directorio_logs, archivo)
-    if os.path.isfile(directorio_logs):
-        filtrar_logs(directorio_logs)
+    Args:
+        file (str): The path to the input file.
+
+    Returns:
+        None
+    """
+    file_write = file.replace(".json", "_filtered.json")
+    with open(file, "r", encoding="utf-8") as f:
+        with open(file_write, "w", encoding="utf-8") as f_out:
+            for line in f:
+                if "GET" in line or "HTTP/1.1" in line:
+                    f_out.write(line)
+
+
+def main():
+    """
+    Process the files in the 'log' directory and filter the ones
+    with a '.json' extension to extract the requests.
+    """
+    directory_logs = os.listdir("log")
+    for folder in directory_logs:
+        print(f"Procesando archivos en la carpeta: {folder}")
+        folder_api = os.path.join("log/", folder)
+        for file in os.listdir(folder_api):
+            if file.endswith(".json"):
+                filter_logs(os.path.join("log/", folder, file))
+
+
+if __name__ == "__main__":
+    main()
