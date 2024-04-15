d7c8224d34f8ec98f5042e94372d2a8d40eacb30
JaviGarc1a <javgaragu1@alum.us.es>
2024-04-15 20:17:33 +0200

    docs: Add typing functions

--- PlanifyAPI-base/PlanifyAPI/rate_limiter.py
+++ PlanifyAPI-base/PlanifyAPI/rate_limiter.py
@@ -4,9 +4,10 @@ This module provides a rate limiter decorator function.
 
 import time
 from functools import wraps
+from typing import Callable, List, Any
 
 
-def rate_limited(calls_per_second):
+def rate_limited(calls_per_second: float) -> Callable:
     """
     Decorator function that limits the rate at which a function can be called.
 
@@ -14,7 +15,7 @@ def rate_limited(calls_per_second):
         calls_per_second (float): The maximum number of calls allowed per second.
 
     Returns:
-        function: The decorated function.
+        Callable: The decorated function.
 
     Example:
         @rate_limited(2)
@@ -24,16 +25,16 @@ def rate_limited(calls_per_second):
         my_function()  # This function can be called at most 2 times per second.
     """
 
-    min_interval = 1.0 / calls_per_second
+    min_interval: float = 1.0 / calls_per_second
 
-    def decorate(func):
-        last_time_called = [0.0]
+    def decorate(func: Callable) -> Callable:
+        last_time_called: List[float] = [0.0]
 
         @wraps(func)
-        def rate_limited_func(*args, **kwargs):
-            elapsed = time.perf_counter() - last_time_called[0]
+        def rate_limited_func(*args, **kwargs) -> Any:
+            elapsed: float = time.perf_counter() - last_time_called[0]
             print("Elapsed:", elapsed)
-            remaining_time = min_interval - elapsed
+            remaining_time: float = min_interval - elapsed
             print("Remaining time:", remaining_time)
             if remaining_time > 0:
                 print("Sleeping...")
